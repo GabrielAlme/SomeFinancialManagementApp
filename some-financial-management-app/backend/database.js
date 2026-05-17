7917789fdcbb9c924671e72d6b2f155ac9aa0653
GabrielAlme <gabealmeemail@gmail.com>
2026-05-17 14:48:22 -0500

    updated database js with a table for accounts and one for plaid tokens

--- some-financial-management-app/backend/database.js
+++ some-financial-management-app/backend/database.js
@@ -13,4 +13,35 @@ db.exec(`
         password TEXT NOT NULL,
         created_at DATETIME DEAFAULT CURRENT_TIMESTAMP
     )
-    `);
+`);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS plaid_tokens (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        access_token TEXT NOT NULL, --encrypetd
+        item_id TEXT NOT NULL, --encrypted
+        institution_name TEXT,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+`);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS accounts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        plaid_token_id INTEGER NOT NULL, --encrypted
+        account_name TEXT,
+        account_type TEXT,
+        balance_current REAL DEFAULT 0,
+        last_updated DATETIME,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (plaid_token_id) REFERENCES plaid_tokens(id)
+    )
+`);
+
+console.log('Database initialized');
+
+module.exports = db;
