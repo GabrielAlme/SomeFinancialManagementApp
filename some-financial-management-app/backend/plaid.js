d6c951e8a0b3d3b2e2957bdfc0ca2def31db2e63
GabrielAlme <gabealmeemail@gmail.com>
2026-06-16 21:10:07 -0500

    added plaid client config

--- some-financial-management-app/backend/plaid.js
+++ some-financial-management-app/backend/plaid.js
@@ -2,3 +2,16 @@ const express = require('express');
 const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
 const db = require('./database');
 const { encrypt, decrypt } = require('./encryption');
+
+const router = express.Router();
+
+//Plaid client
+const config = new Configuration({
+    basePath: PlaidEnvironments[process.env.PLAID_ENV],
+    baseOptions: {
+        headers: {
+            'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+            'PLAID_SECRET': process.env.PLAID_SECRET,
+        },
+    },
+});
