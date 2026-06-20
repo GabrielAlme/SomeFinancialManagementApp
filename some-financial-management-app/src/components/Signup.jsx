2b00dfe4db76f8f1c9397a7b2071199ac3a0fa55
GabrielAlme <gabealmeemail@gmail.com>
2026-06-20 14:56:18 -0500

    finished signup page

--- some-financial-management-app/src/components/Signup.jsx
+++ some-financial-management-app/src/components/Signup.jsx
@@ -32,4 +32,35 @@ function Signup({setToken, setShowSignup}) {
 
         }
     };
+
+    return (
+        <div className="login-container">
+            <h1>Some Financial Manager</h1>
+
+            {error && <p className="error">{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label>Username:</label>
+                    <input type="text" value={identifier} onChange={(e) => setUsername(e.target.value)}/>
+                </div>
+                <div className="input-group">
+                    <label>Email:</label>
+                    <input type="text" value={identifier} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className="input-group">
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+
+                <button type="submit">Signup</button>
+            </form>
+
+            <p onClick={() => setShowSignup(false)} className="toggle">
+                Already have an account? Login
+            </p>
+        </div>
+    );
 }
+
+export default Signup;
