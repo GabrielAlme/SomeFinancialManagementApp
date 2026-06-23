97fefaccb51d746847a941df50b82a24286d6c56
GabrielAlme <gabealmeemail@gmail.com>
2026-06-23 15:07:06 -0500

    added an api url var for keeping the codespace clean

--- some-financial-management-app/src/components/panels/AccountsPanel.jsx
+++ some-financial-management-app/src/components/panels/AccountsPanel.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { usePlaidLink } from 'react-plaid-link';
+import API_URL from '../../config';
 
 function AccountsPanel ({ token }) {
     const [linkToken, setLinkToken] = useState(null);
     const [accounts, setAccounts] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
+
+    const fetchAccounts = async () => {
+        try  {
+            const response = await fetch(`${API_URL}/plaid/accounts/0`)
+        } catch {
+            
+        }
+    }
 }
